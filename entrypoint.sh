8d1fc32cb91c6741ccdc754b8cea762e9ab91f60
khrist14n <khrist14n@gmail.com>
2023-03-11 12:39:10 -0500

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,33 +3,30 @@ set -e
 
 PATH=$1
 
-function check() {
-    GIT_PREVIOUS_COMMIT=`$(git rev-parse --short "HEAD^")`
-    GIT_COMMIT=`$(git rev-parse --short HEAD)`
+GIT_PREVIOUS_COMMIT=$(git rev-parse --short "HEAD^")
+GIT_COMMIT=$(git rev-parse --short HEAD)
 
-    DIR_PATH=$PATH
-    if [ ! -d "$DIR_PATH" ]; then
-        ##echo "Directory '$DIR_PATH' not exists"
-        exit 1
-    fi
+DIR_PATH=$PATH
 
-    if [ -z "$GIT_COMMIT" ]; then
-        ##echo "No current commit... fail"
-        exit 1
-    fi
+if [ ! -d "$DIR_PATH" ]; then
+    exit 1
+fi
 
-    if [ -z "$GIT_PREVIOUS_COMMIT" ]; then
-        ##echo "No previous commit, files are changed!"
-        exit 0
-    fi
+if [ -z "$GIT_COMMIT" ]; then
+    exit 1
+fi
 
-    CHANGED=`git diff --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT $DIR_PATH`
+if [ -z "$GIT_PREVIOUS_COMMIT" ]; then
+    exit 0
+fi
 
-    if [ -z "$CHANGED" ]; then
-            echo "0"
-    else
-            echo "1"
-    fi
-}
+CHANGED=`git diff --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT $DIR_PATH`
 
-echo ::set-output name=changed::$(check)
+OUTPUT="0"
+if [ -z "$CHANGED" ]; then
+    OUTPUT="0"
+else
+    OUTPUT="1"
+fi
+
+echo ::set-output name=changed::$(echo $OUTPUT)
