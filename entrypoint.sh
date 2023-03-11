b59d1e28734953b5edc36e3ce2d08b22945d27fa
khrist14n <khrist14n@gmail.com>
2023-03-11 12:17:36 -0500

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,23 +4,23 @@ set -e
 PATH=$1
 
 function check() {
-    GIT_PREVIOUS_COMMIT=$(git rev-parse --short "HEAD^")
-    GIT_COMMIT=$(git rev-parse --short HEAD)
+    GIT_PREVIOUS_COMMIT=`$(git rev-parse --short "HEAD^")`
+    GIT_COMMIT=`$(git rev-parse --short HEAD)`
 
     DIR_PATH=$PATH
     if [ ! -d "$DIR_PATH" ]; then
         ##echo "Directory '$DIR_PATH' not exists"
-        echo "0"
+        exit 1
     fi
 
     if [ -z "$GIT_COMMIT" ]; then
         ##echo "No current commit... fail"
-        echo "0"
+        exit 1
     fi
 
     if [ -z "$GIT_PREVIOUS_COMMIT" ]; then
         ##echo "No previous commit, files are changed!"
-        echo "0"
+        exit 0
     fi
 
     CHANGED=`git diff --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT $DIR_PATH`
